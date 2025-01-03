d7ae10b243ae2e7440a4349d258e485a85c04bc9
VARKanae <olimuotek@wp.pl>
2025-01-03 23:23:22 +0100

    oparte na ruchu gracza
    Signed-off-by: VARKanae <olimuotek@wp.pl>

--- tictoe.py
+++ tictoe.py
@@ -8,15 +8,18 @@ class Korzen:
         self.dzieci.append(kij)
 
     def dziecko_z_nazwa(self, name):
+        wynik: Korzen = None
         for dzieck in self.dzieci:
             if dzieck.nazwa == name:
-                return dzieck
+                wynik = dzieck
+        return wynik
+    
 
 class Patyk(Korzen): #Że w sensie gałąź. Dziedziczenie z korzenia ma sens tylko obiektowo
     def __init__(self, name, parent):
         self.rodzic = parent
         super().__init__(name)
-        super().add(self)
+        parent.add(self)
 
 class Lisc(Patyk):
     def __init__(self, name, parent, heur):
@@ -35,17 +38,19 @@ def print_ruch():
     print("\033[95mPola:")
     print("0-0 | 0-1 | 0-2")
     print("---------------")
-    print("0-0 | 0-1 | 0-2")
+    print("1-0 | 1-1 | 1-2")
     print("---------------")
-    print("2-0 | 0-1 | 0-2\033[0m")
+    print("2-0 | 2-1 | 2-2\033[0m")
 
 ostatni = punkt_startowy
 def wykonaj(r, symb):
     global mat
     global m_ruchy
     global ostatni
+    global 勝者
     
     ostatni = ostatni.dziecko_z_nazwa(r)
+    m_ruchy.remove(r)
 
     x,y = r.split('-')
     x = int(x); y = int(y)
@@ -77,28 +82,80 @@ def runda_gracza():
     
     wykonaj(r_gracza, gracz)
 
+def minimax(galaz, maxxing):
+    if isinstance(galaz, Lisc):
+        return galaz.value
+
+    if maxxing:
+        wartosc = -99999
+        for child in galaz.dzieci:
+            wartosc += max(wartosc, minimax(child, False))
+            x,y = map(int, galaz.nazwa.split('-'))
+            gracza = 0
+            if x == 1 == y:
+                gracza += 1 if mat[0][0] == gracz else 0
+                gracza += 1 if mat[2][2] == gracz else 0
+                gracza += 1 if mat[0][2] == gracz else 0
+                gracza += 1 if mat[2][0] == gracz else 0
+            for n in range(3):
+                gracza += 1 if mat[n][y] == gracz else 0
+                gracza += 1 if mat[x][n] == gracz else 0
+            wartosc += gracza
+        return wartosc
+
+    else: 
+        wart = 99999
+        for child in galaz.dzieci:
+            wart -= min(wart, minimax(child, True))
+            x,y = map(int, galaz.nazwa.split('-'))
+            gracza = 0
+            if x == 1 == y:
+                gracza -= 1 if mat[0][0] == gracz else 0
+                gracza -= 1 if mat[2][2] == gracz else 0
+                gracza -= 1 if mat[0][2] == gracz else 0
+                gracza -= 1 if mat[2][0] == gracz else 0
+            for n in range(3):
+                gracza -= 1 if mat[n][y] == gracz else 0
+                gracza -= 1 if mat[x][n] == gracz else 0
+            wart += gracza
+        return wart
 
 while gracz != 'O' and gracz != 'X':
     gracz = input("\033[33mGracz jest kółkiem czy krzyżykiem? [O/X]\033[0m\n>\t").strip().upper()
     
-if gracz == 'O':
-    wykonaj("0-0", 'X')
-    
+def runda_AI():
+    global m_ruchy
 
-#Po zdefiniowaniu gracza, budujemy drzewo.
-utworzone_lvl = [0, [], [], [],[], [], [],[], [], []]
+    opcje = []
+    for mozliwy in ostatni.dzieci:
+        opcje.append((mozliwy.nazwa, minimax(mozliwy, True)))
+        
+    print(opcje)
+    ruch = max(opcje, key = lambda x: x[1])[0]
+    niegracz = 'O' if gracz == 'X' else 'X'
+    wykonaj(ruch, niegracz)
+
+
+def print_drzew(node, poziom, count):
+    lista = []
+    for n in node.dzieci:
+        lista.append(n.nazwa)
+        print(lista)
+        if poziom > 1:
+            print_drzew(n, poziom - 1, count + 1)
 
 with open("./tictactoe_games.csv", 'r') as plik:
+    print("Generowanie drzewa decyzyjnego z pliku...")
     for (i, gra) in enumerate(plik.readlines()):
+        gra = gra[:-1]
         if i == 0: 
             continue
 
         poprzednik: Korzen = punkt_startowy
-        for (n, ruch) in enumerate(gra.split(',')):
-
+        giera = gra.split(',')
+        for (n, ruch) in enumerate(giera):
             if ruch == '---':
-                break
-
+                continue
             if n == 0:
                 match ruch:
                     case '-': ostatnik = 0
@@ -108,14 +165,28 @@ with open("./tictactoe_games.csv", 'r') as plik:
                         ostatnik = 1 if gracz == 'X' else -1
                 continue
 
-            galaz = Patyk(ruch, poprzednik) if n != 9 else Lisc(ruch, poprzednik, ostatnik)
+            istniejacy = poprzednik.dziecko_z_nazwa(ruch)
+            if istniejacy is not None:
+                galaz = istniejacy
+            else:
+                if n == 9 or giera[n+1] == "---":
+                    galaz = Lisc(ruch, poprzednik, ostatnik)
+                else:
+                    galaz = Patyk(ruch, poprzednik)
             poprzednik = galaz
                 
+
+if gracz == 'O':
+    wykonaj("0-0", 'X')
 
 #Pętla rozgrywki
 while len(m_ruchy) > 0 and 勝者 == '':
     runda_gracza()
+    if 勝者 != '' or len(m_ruchy) == 0:
+        break
     runda_AI()
 
 if len(m_ruchy) == 0:
     print("Remis")
+else:
+    print(f"\033[92mZwycięzcą jest {勝者}\033[0m")
